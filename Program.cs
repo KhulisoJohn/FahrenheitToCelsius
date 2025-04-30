d6ccd3fe07b32e0285876b797b550b7f98b7dab0
khulysojohn <khuliso.thavhiwa@techbridlefoundation.org>
2025-04-30 09:12:37 +0200

    initial commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace FahrenheitToCelsius {
+
+    class Program {
+
+        static void Main(string[] args) {
+
+            Console.Write("Enter temperature in Fahrenheit: ");
+            int fahrenheit = Convert.ToInt32(Console.ReadLine());
+            double celsius = (fahrenheit - 32) * (5.0 / 9.0);
+            Console.WriteLine($"The temperature in Celsius is: {celsius:F2}");
+        }
+    }
+}
